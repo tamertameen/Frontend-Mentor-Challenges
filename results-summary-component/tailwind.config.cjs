6f7ae5286fe54bf56a4e0171cae0640a049094bf
Tamer Elhamalawy <tamer.tameen@gmail.com>
2024-01-11 18:09:44 +0400

    setup vite

---
+++ results-summary-component/tailwind.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{html, css, js}'],
+  theme: {
+    screens: {
+      sm: '375px',
+      xl: '1440px'
+    },
+    colors: {
+      Light_red: 'hsl(0, 100%, 67%)',
+      Orangey_yellow: 'hsl(39, 100%, 56%)',
+      Green_teal: 'hsl(166, 100%, 37%)',
+      Cobalt_blue: 'hsl(234, 85%, 45%)',
+
+      /* Gradients */
+      Light_slate_blue: 'hsl(252, 100%, 67%)',
+      Light_royal_blue: 'hsl(241, 81%, 54%)',
+      Violet_blue: 'hsla(256, 72%, 46%, 1)',
+      Persian_blue: 'hsla(241, 72%, 46%, 0)',
+
+      /* Neutral */
+
+      White: 'hsl(0, 0%, 100%)',
+      Pale_blue: 'hsl(221, 100%, 96%)',
+      Light_lavender: 'hsl(241, 100%, 89%)',
+      Dark_gray_blue: 'hsl(224, 30%, 27%)'
+    },
+    fontFamily: {
+      sans: ['Hanken Grotesk', 'sans-serif']
+    }
+  },
+  plugins: []
+};
+
