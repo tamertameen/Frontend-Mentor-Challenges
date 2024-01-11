b6053e00e88865e2323762d0043c26bf633f01a4
Tamer Elhamalawy <tamer.tameen@gmail.com>
2024-01-11 15:57:51 +0400

    updated nodemon config

--- results-summary-component/server.js
+++ results-summary-component/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 const dir = `${__dirname}/`;
 
 app.get("*", (req, res) => {
   res.sendFile(dir + "index.html");
 });
 
-app.listen(3000);
+app.listen(port, () => console.log(`I am listening on port ${port}!`));
