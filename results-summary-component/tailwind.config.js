1f5f29d162698f2d5a5c70913fba4546d2a374d6
Tamer Elhamalawy <tamer.tameen@gmail.com>
2024-01-18 18:37:38 +0400

    fixed colours and opacity

--- results-summary-component/tailwind.config.js
+++ results-summary-component/tailwind.config.js
@@ -1,33 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ['./*.{html,js}'],
   theme: {
     screens: {
-      sm: "375px",
-      xl: "1440px",
+      sm: '375px',
+      xl: '1440px',
     },
     colors: {
-      Light_red: "hsl(0, 100%, 67%)",
-      Orangey_yellow: "hsl(39, 100%, 56%)",
-      Green_teal: "hsl(166, 100%, 37%)",
-      Cobalt_blue: "hsl(234, 85%, 45%)",
+      /* Test Opacity*/
+      test_red: 'hsl(var(--test-red))',
+      test_red_1: 'hsl(var(--test-red-1))',
+      test_red_0: 'hsl(var(--test-red-0))',
+
+      /* Primary */
+      Primary_Light_Red: 'hsl(var(--light-red))',
+      Primary_Orangey_Yellow: 'hsl(var(--orangey-yellow))',
+      Primary_Green_Teal: 'hsl(var(--green-teal))',
+      Primary_Cobalt_Blue: 'hsl(var(--cobalt-blue))',
 
       /* Gradients */
-      Light_slate_blue: "hsl(252, 100%, 67%)",
-      Light_royal_blue: "hsl(241, 81%, 54%)",
-      Violet_blue: "hsla(256, 72%, 46%, 1)",
-      Persian_blue: "hsla(241, 72%, 46%, 0)",
+      Gradient_Slate_Blue: 'hsl(var(--light-slate-blue))',
+      Gradient_Royal_Blue: 'hsl(var(--light-royal-blue))',
+      Gradient_Violet_Blue: 'hsl(var(--violet-blue))',
+      Gradient_Persian_Blue: 'hsl(var(--persian-blue))',
 
       /* Neutral */
 
-      White: "hsl(0, 0%, 100%)",
-      Pale_blue: "hsl(221, 100%, 96%)",
-      Light_lavender: "hsl(241, 100%, 89%)",
-      Dark_gray_blue: "hsl(224, 30%, 27%)",
+      Neutral_White: 'hsl(var(--white))',
+      Neutral_Pale_Blue: 'hsl(var(--pale-blue))',
+      Neutral_Light_Lavender: 'hsl(var(--light-lavender))',
+      Neutral_Dark_Gray_Blue: 'hsl(var(--dark-gray-blue))',
     },
-    fontFamily: {
-      sans: ["Hanken Grotesk", "sans-serif"],
+    extend: {
+      fontFamily: {
+        Hanken: 'Hanken Grotesk, sans-serif',
+      },
     },
   },
   plugins: [],
-};
+}
