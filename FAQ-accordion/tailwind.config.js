f73f7851f463bf8ae83d5631cdcee940d209e604
Tamer Elhamalawy <tamer.tameen@gmail.com>
2024-01-25 16:02:41 +0400

    styling completed

---
+++ FAQ-accordion/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        clr_neutral: 'hsl(var(--clr-neutral-hsl) / <alpha-value>)',
+        clr_primary: 'hsl(var(--clr-primary-hsl) / <alpha-value>)',
+        clr_secondary: 'hsl(var(--clr-secondary-hsl) / <alpha-value>)',
+        clr_accent: 'hsl(var(--clr-accent-hsl) / <alpha-value>)',
+      },
+
+      fontFamily: {
+        WorkSans: 'Work Sans, sans-serif',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '100vw',
+      },
+      boxShadow: {
+        neutral_4: '0.5rem 1rem 3rem hsl(224, 30%, 27%, 0.2)',
+      },
+      fontSize: {
+        fs_700: '1.75rem',
+        fs_900: '5rem',
+      },
+      width: {
+        custom: '36rem',
+      },
+    },
+  },
+  plugins: [],
+}
